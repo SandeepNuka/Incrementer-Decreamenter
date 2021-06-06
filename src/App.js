68d80b4bf9f9d9db647d58abc0e44c67e8fdcc71
Sandeep Nuka <sandeepnuka21@gmail.com>
2021-06-06 10:12:07 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [state, setState] = useState(0);
+
+  const Onsubmit = () => {
+    setState(state + 1);
+  };
+
+  const Onsubmit1 = () => {
+    setState(state - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="  container vh-100   ">
+      <div className="row  m-5 ">
+        <div className=" col">
+          <button onClick={Onsubmit1} className="btn btn-secondary">
+            <h1>-</h1>
+          </button>
+        </div>
+        <div className="col">
+          <h1> {state}</h1>
+        </div>
+        <div className=" col">
+          <button onClick={Onsubmit} className="btn btn-primary   ">
+            <h1>+</h1>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
